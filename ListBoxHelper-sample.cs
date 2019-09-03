ce97abb83a16e532bfe2f723f21a33f57f1460bf
arters <arters@users.noreply.github.com>
2019-09-03 11:36:04 +0800

    Create ListBoxHelper-sample.cs

---
+++ ListBoxHelper-sample.cs
@@ -0,0 +1,16 @@
+/**
+ * ListBoxHelper - ListBoxHelper is a helper class that provides methods that
+ * help ListBox control in Windows Forms. 
+ * https://github.com/arters/Csharp-helper-class/blob/master/ListBoxHelper.cs
+ *
+ * @author Jwu
+ * @license MIT
+ *
+ * @version 1.0.0
+ */
+ 
+ 
+// 循序找出 ListBox 中符合關鍵字的項目
+// 範例：
+ListBoxHelper.Find(listBox1, "關鍵字");
+ListBoxHelper.Find(listBox1, QueryTextBox.Text);
